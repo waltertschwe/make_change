f1e7b3ee2eba4241b15751d0b394712c90d2abdb
Walter Schweitzer <wschweitzer00@gmail.com>
2021-12-13 09:44:42 -0500

    add input values with negative test

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import decimal
 from services.make_change import MakeChange
 from services.errors import NoChangeRequired, ValueTooSmallError
 
@@ -5,15 +6,19 @@ if __name__ == "__main__":
     """
     Start MakeChange Application.
     """
-    total_cost = "15.00"
-    paid = "15.50"
 
+   
+    total_cost = input("Enter Total Cost: ")
+    paid = input("Enter Amount Provided: ")
+    
     make_change = MakeChange(total_cost, paid)
     try:
         change, total_change = make_change.get_change()
         print(f"Total change owned: {total_change}")
         for coin in change.items():
             print(f"Denomination = {coin[0]} Number Returned = {coin[1]}")
+    except decimal.InvalidOperation:
+        print("Bad input data.  Please try again.")
     except ValueTooSmallError:
         print("Customer still owes more money...")
     except NoChangeRequired:
