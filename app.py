624ca8afc29316d736db7263517a24ee5997fa67
Walter Schweitzer <wschweitzer00@gmail.com>
2021-12-16 05:12:33 -0500

    clean up output

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import decimal
+
 from services.make_change import MakeChange
 from services.errors import NoChangeRequired, ValueTooSmallError
 
@@ -15,7 +16,7 @@ if __name__ == "__main__":
         change, total_change = make_change.get_change()
         print(f"Total change owned: {total_change}")
         for coin in change.items():
-            print(f"Denomination = {coin[0]} Number Returned = {coin[1]}")
+            print(f"{coin[0]} Number Returned = {coin[1]}")
     except decimal.InvalidOperation:
         print("Bad input data.  Please try again.")
     except ValueTooSmallError:
